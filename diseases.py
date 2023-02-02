0830a6b922194cff0a33d827c5090a3be53b6823
ismaildrcn <ismaildurcan40@gmail.com>
2023-02-03 00:05:47 +0300

    Added Brain Tumor Detection Class

--- diseases.py
+++ diseases.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+from functools import reduce
 from keras.models import load_model
 
 class PneumoniaDetect():
@@ -15,4 +16,20 @@ class PneumoniaDetect():
     def detect(self, img):
         model = load_model('weights/pneumonia.h5')
         result = model.predict(img)[0][0] * 100
+        return result
+
+
+class brainTumorDetect():
+    def Preprocessing(self, img):
+        imgSize = 100
+        imgRead = cv2.imread(img)
+        img = cv2.resize(imgRead, (imgSize, imgSize))
+        img = np.array(img)
+        img = img.reshape(-1, imgSize, imgSize, 3)
+        return img
+
+    def detect(self, img):
+        model = load_model('weights/brainTumor.h5')
+        result = model.predict(img)
+        result = [x * 100 for x in result[0]]
         return result
